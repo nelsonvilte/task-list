fdf855750fd07f58f3535746d0a064d46e985ef0
Nelson Vilte <nelsonvilte@gmail.com>
2022-01-21 00:34:06 -0300

    Lista de items

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -17,12 +17,12 @@ export const TASKS: Task[] =[
         text:"Hacer las compras de fin de año",
         day:"Diciembre 29 a las 17:00",
         reminder: true
-      },
-      {
+    },
+    {
         id:4,
         text:"Investigar sobre Bootstrap",
         day:"Diciembre 30 a las 19:00",
         reminder: true
-      },
+    },
 
 ]
